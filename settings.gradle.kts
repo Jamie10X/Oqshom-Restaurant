ff877cb00373123f009df20dd0e27c4ea579f2d1
Jamie10X <jamshidboynazarov0@gmail.com>
2023-10-10 17:55:45 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Oqshom_restaurant"
+include(":app")
